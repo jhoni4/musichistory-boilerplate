75fa9e62cc1b13b571a732b58002a3febee34c0e
yonatan molla <jhoni42002@yahoo.com>
2016-08-19 14:19:52 -0500

    more button worked

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # Music History
 Instructions
 
-Part One
+This one's simple.
 
-Read from local JSON file with an XHR.
-Loop over results and inject into Music History list view.
-Add delete button DOM to each row and, when it is clicked, delete the entire row in the DOM.
-Part Two
-
-Take your music and split it into two JSON file instead of them all living on one file.
-Add a button at the bottom of your music list and label it "More >".
-Load the songs from the first list and inject the DOM into the document as you've already done.
-When the user clicks that button, load the songs from the second JSON file and append them to the bottom of the existing music, but before the More button.
+Implement jQuery in your Music History code. Every innerHTML, getElementById, getElementByClassName, event listener and XHR request. Convert 'em all.
--- xhr.js
+++ xhr.js
@@ -2,103 +2,106 @@
     var allSongs = [];
     var moreSongs = [];
 
-function getSongInfo (callback) {
-  var xhr = new XMLHttpRequest()
-  xhr.open('GET', 'allSongs.json')
-  xhr.addEventListener("load", function(evt) {
-  allSongs = JSON.parse(this.responseText);
+$(function () {
 
-  callback(allSongs)
-     })
-  xhr.send()
-}
-getSongInfo(showSongs);
 
-function getMoreSongInfo (callback) {
-        var xhr = new XMLHttpRequest()
-        xhr.open('GET', 'moreSongs.json')
-        xhr.addEventListener("load", function(evt) {
-        moreSongs = JSON.parse(this.responseText);
+  $.getJSON('allSongs.json' )
+    .then((data1) => {
+      allSongs = data1;
+      // console.log('data1', data1.songs)
+      showSongs(data1);
 
-        callback(moreSongs)
-         })
-        xhr.send()
-    }
-    getMoreSongInfo(showMoreSongs);
+      $.get('moreSongs.json')
+      .then((data2) => {
+        moreSongs = data2;
+        // console.log('data2', moreSongs.songs[0].title)
+        $("#moreButton").click(function(e) {
+          showSongs(data2);
 
-function showSongs (allSongs) {
-  var output = "";
-  var output = document.getElementById("yellow-box");
-  var allSongsString = "";
-// Looping though all array of songs
+        })
+      })
+    })
 
-  for (var i = 0; i < allSongs.songs.length; i++) {
-     var each = allSongs.songs[i];
-      // alert(allSongs.songs[i]);
-     var songName = each.title;
-     var artistName = each.artist;
-     var albumName = each.album;
-    allSongsString +=
-     `<div class="song">
-        <h2 class="name">${songName}</h2>
-        <ul>
-          <li class="artist">${artistName}</li>
-          <li>|</li>
-          <li class="album">${albumName}</li>
-          <li>|</li>
-          <li class="genre">Genre</li>
-        </ul>
-        <span><button type="button" class="deleteButtoN" name="delete">Delete</button></span>
-      </div>`;
-  }
-   output.innerHTML = allSongsString;
+  // var getMoreSongInfo = $.when($.get('moreSongs.json'))
+  //   .then((data2) => {
+  //     moreSongs = data2;
+  //     // console.log('data2', moreSongs.songs[0].title)
 
-   var deleteButtons = document.getElementsByClassName("deleteButtoN");
-   for (var j = 0; j < deleteButtons.length; j++) {
-   deleteButtons[j].addEventListener("click", function (evt) {
-    console.log("delete button clicked")
-     evt.target.parentElement.parentElement.remove();
-  });
-   }
+  //     showMoreSongs(data2);
 
-}
+  //   })
 
-function showMoreSongs (moreSongs) {
-    // var output1 = "";
-    var output1 = document.getElementById("more-box");
-    var moreButton = document.getElementById("moreButton");
-    var moreSongsString = "";
+
+  function showSongs (allSongs) {
+    var allSongsString = "";
 
   // Looping though all array of songs
-  moreButton.addEventListener("click", function (evt){
-    console.log("more button clicked")
-    for (var j = 0; j < moreSongs.songs.length; j++) {
-       var each = moreSongs.songs[j];
+
+    for (var i = 0; i < allSongs.songs.length; i++) {
+       var each = allSongs.songs[i];
+        // alert(allSongs.songs[i]);
        var songName = each.title;
        var artistName = each.artist;
        var albumName = each.album;
-      moreSongsString +=
-      `<div class="song">
-        <h2 class="name">${songName}</h2>
-        <ul>
-          <li class="artist">${artistName}</li>
-          <li>|</li>
-          <li class="album">${albumName}</li>
-          <li>|</li>
-          <li class="genre">Genre</li>
-        </ul>
-        <span><button type="button" class="deleteButtoN" name="delete">Delete</button></span>
-      </div>`;
-  }
-   output1.innerHTML = moreSongsString;
+       var Genre = each.genre;
+      allSongsString +=
+       `<div class="song">
+          <h2 class="name">${songName}</h2>
+          <ul>
+            <li class="artist">${artistName}</li>
+            <li>|</li>
+            <li class="album">${albumName}</li>
+            <li>|</li>
+            <li class="genre">${Genre}</li>
+          </ul>
+          <span><button type="button" class="deleteButtoN" name="delete">Delete</button></span>
+        </div>`;
+    }
+     $("#yellow-box").append(allSongsString);
 
-   var deleteButtons = document.getElementsByClassName("deleteButtoN");
-   for (var j = 0; j < deleteButtons.length; j++) {
-   deleteButtons[j].addEventListener("click", function (evt) {
-    console.log("delete button clicked")
-     evt.target.parentElement.parentElement.remove();
-  });
-   }
- });
-}
+
+     $(".deleteButtoN").click(function (evt) {
+      // console.log("delete button clicked")
+       this.parentElement.parentElement.remove();
+    });
+
+  }
+
+});
+  // function showMoreSongs (moreSongs) {
+  //     var moreSongsString = "";
+
+  //   // Looping though all array of songs
+  //   $("#moreButton").click(function(e) {
+  //       moreSongsString = "";
+  //       // console.log("more button clicked")
+  //       for (var j = 0; j < moreSongs.songs.length; j++) {
+  //          var each = moreSongs.songs[j];
+  //          var songName = each.title;
+  //          var artistName = each.artist;
+  //          var albumName = each.album;
+  //          var Genre = each.genre;
+  //         moreSongsString +=
+  //         `<div class="song">
+  //           <h2 class="name">${songName}</h2>
+  //           <ul>
+  //             <li class="artist">${artistName}</li>
+  //             <li>|</li>
+  //             <li class="album">${albumName}</li>
+  //             <li>|</li>
+  //             <li class="genre">${Genre}</li>
+  //           </ul>
+  //           <span><button type="button" class="deleteButton2" name="delete">Delete</button></span>
+  //         </div>`;
+  //     }
+  //      $("#more-box").append(moreSongsString);
+
+  //      $(".deleteButton2").click(function (evt) {
+  //       // console.log("delete button clicked")
+  //        this.parentElement.parentElement.remove();
+  //     });
+
+  //      // $("#moreButton").remove();
+  //    });
+  // }
 
